564810b41f79aab9e68ed1f3c9564fa37b990808
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2024-10-26 07:26:47 +0900

    feat: add `YmmeUtil.Ymm4` project

---
+++ src/YmmeUtil.Ymm4/TaskbarUtil.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Shell;
+
+namespace YmmeUtil.Ymm4;
+
+public static class TaskbarUtil
+{
+	public static TaskbarItemInfo GetMainTaskbarInfo()
+	{
+		var main = Application.Current.MainWindow;
+		main.TaskbarItemInfo ??= new TaskbarItemInfo();
+		return main.TaskbarItemInfo;
+	}
+
+	public static void StartIndeterminate(TaskbarItemInfo? taskbar = null)
+	{
+		taskbar ??= GetMainTaskbarInfo();
+
+		taskbar.ProgressState = TaskbarItemProgressState.Indeterminate;
+	}
+
+	public static void PauseIndeterminate(TaskbarItemInfo? taskbar = null)
+	{
+		taskbar ??= GetMainTaskbarInfo();
+
+		taskbar.ProgressState = TaskbarItemProgressState.Paused;
+	}
+
+	public static void FinishIndeterminate(TaskbarItemInfo? taskbar = null)
+	{
+		taskbar ??= GetMainTaskbarInfo();
+
+		taskbar.ProgressState = TaskbarItemProgressState.None;
+	}
+}
