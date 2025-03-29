1617af423bca0b68dc943e248d66cf5a7d9a2eb7
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2025-03-29 15:57:19 +0900

    feat: add VSCode launch and task configurations for sandbox plugins

--- sandbox/plugin/Common/Common.cs
+++ sandbox/plugin/Common/Common.cs
@@ -1,8 +1,13 @@
-﻿using YukkuriMovieMaker.Plugin;
+﻿using System.Reflection;
+
+using YukkuriMovieMaker.Plugin;
 
 namespace YmmeUtil.Sandbox;
 
+[PluginDetails(AuthorName = "InuInu", ContentId = "")]
 public class Common : IPlugin
 {
 	public string Name { get; } = "Sandbox.Common";
+	public PluginDetailsAttribute Details =>
+		GetType().GetCustomAttribute<PluginDetailsAttribute>() ?? new();
 }
--- sandbox/plugin/WithYmm4/WithYmm4.cs
+++ sandbox/plugin/WithYmm4/WithYmm4.cs
@@ -1,8 +1,13 @@
-﻿using YukkuriMovieMaker.Plugin;
+﻿using System.Reflection;
+
+using YukkuriMovieMaker.Plugin;
 
 namespace YmmeUtil.Sandbox;
 
+[PluginDetails(AuthorName = "InuInu", ContentId = "")]
 public class WithYmm4 : IPlugin
 {
 	public string Name { get; } = "Sandbox.WithYmm4";
+	public PluginDetailsAttribute Details =>
+		GetType().GetCustomAttribute<PluginDetailsAttribute>() ?? new();
 }
