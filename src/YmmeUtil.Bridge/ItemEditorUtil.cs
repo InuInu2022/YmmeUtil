d3459fa0a30a0f13d3ad57fdfa45ef43f2db87f4
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2025-09-03 07:21:25 +0900

    feat: TryGetItemEditorメソッドの引数を修正し、nullチェックを改善

--- src/YmmeUtil.Bridge/ItemEditorUtil.cs
+++ src/YmmeUtil.Bridge/ItemEditorUtil.cs
@@ -1,3 +1,7 @@
+using System.Diagnostics.CodeAnalysis;
+
+using YmmeUtil.Ymm4;
+
 namespace YmmeUtil.Bridge;
 
 /// <summary>
@@ -5,30 +9,65 @@ namespace YmmeUtil.Bridge;
 /// </summary>
 public static class ItemEditorUtil
 {
-	public static bool TryGetItemEditor(out WrapItemEditor? itemEditor)
+
+	public static bool TryGetItemEditor(
+		[NotNullWhen(true)]
+		out WrapItemEditor? itemEditor)
 	{
 		itemEditor = default;
-		var mainWinVM = TimelineUtil.GetMainViewModel();
-		if (mainWinVM is null)
+
+		if (Ymm4Version.HasDocked)
 		{
-			return false;
-		}
+			var mainWinVM = TimelineUtil.GetMainViewModel();
+			if (mainWinVM is null)
+			{
+				return false;
+			}
 
-		var itemEditorVM = Internal.Reflect.GetProp(mainWinVM, "ItemEditorAreaViewModel");
-		if (itemEditorVM is null)
+			var itemEditorVM = Internal.Reflect.GetField(
+				mainWinVM,
+				"itemEditorAreaViewModel",
+				isPrivate: true
+			);
+			if (itemEditorVM is null)
+			{
+				return false;
+			}
+
+			var vm = Internal.Reflect.GetProp(
+				itemEditorVM,
+				"ViewModel"
+			);
+			if (vm is null) return false;
+
+			itemEditor = new WrapItemEditor(vm);
+
+			return true;
+		}
+		else
 		{
-			return false;
+			var mainWinVM = TimelineUtil.GetMainViewModel();
+			if (mainWinVM is null)
+			{
+				return false;
+			}
+
+			var itemEditorVM = Internal.Reflect.GetProp(mainWinVM, "ItemEditorAreaViewModel");
+			if (itemEditorVM is null)
+			{
+				return false;
+			}
+
+			var vm = Internal.Reflect.GetProp(itemEditorVM, "ViewModel");
+			if (vm is null)
+				return false;
+
+			var vmValue = Internal.Reflect.GetProp(vm, "Value");
+			if (vmValue is null)
+				return false;
+
+			itemEditor = new WrapItemEditor(vmValue);
+			return true;
 		}
-
-		var vm = Internal.Reflect.GetProp(itemEditorVM, "ViewModel");
-		if (vm is null)
-			return false;
-
-		var vmValue = Internal.Reflect.GetProp(vm, "Value");
-		if (vmValue is null)
-			return false;
-
-		itemEditor = new WrapItemEditor(vmValue);
-		return true;
 	}
 }
